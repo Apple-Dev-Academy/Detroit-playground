66062192751f24aef2bfffbd78aa50c654df856c
HadiChaaban <chaabanh@umich.edu>
2022-09-29 10:23:33 -0400

    HenryFord Story finished

--- Decide Detroit.playground/Contents.swift
+++ Decide Detroit.playground/Contents.swift
@@ -17,12 +17,6 @@ class Choices: Identifiable {
     }
 }
 
-//var choiceTree = ["In 1701 you found DETROIT with 200 of your frenchmen" :
-//                    ["Kill the Natives" : ["You enter war with the natives and lose half your men": ["Choice 1"]],
-//                     "Come together with the natives" : "You sign a treaty with the natives for economic growth purposes",
-//                     "Call the British" : "You enter war with the natives and lose half your men"]
-//                    ]
-
 func createChoiceTree() -> Choices {
     let og = Choices(title: "", result: "In 1701 you found DETROIT with 200 of your frenchmen")
     og.addChoice(newtitle: "Kill the Natives", newresult: "You enter war with the natives and lose half your men")
@@ -35,7 +29,23 @@ func createChoiceTree() -> Choices {
 }
 
 func createHenryFordTree() -> Choices {
-    
+    let og = Choices(title: "", result: "In 1882, you, a young Henry Ford, finishes your first apprenticeship in the heart of DETROIT")
+    og.addChoice(newtitle: "Go work for Thomas Edison", newresult: "Edison stock shoots up by double")
+    og.choiceArr[0].addChoice(newtitle: "Continue working for Edison", newresult: "Edison suggests pivoting to something new since illumination is a no longer profitable field")
+    og.choiceArr[0].choiceArr[0].addChoice(newtitle: "Pivot to cars", newresult: "You start one of the biggest car companies in the world and change Detroit forver")
+    og.choiceArr[0].choiceArr[0].addChoice(newtitle: "Pivot to medicine", newresult: "You cure cancer and become immortal, the entire world bows to your knees")
+    og.choiceArr[0].addChoice(newtitle: "Quit and start your own company", newresult: "Edison is ANGERED and hires a hitman against you!")
+    og.choiceArr[0].choiceArr[1].addChoice(newtitle: "Move to Canada and hide out", newresult: "You stay in hiding for the rest of your life, but its ok because timbits make it worth it")
+    og.choiceArr[0].choiceArr[1].addChoice(newtitle: "Hire a hitman back", newresult: "Both of you are killed by eachother")
+    og.addChoice(newtitle: "Focus on working on the farm with your dear wife, Clara", newresult: "Your old boss reaches out to you about a new opportunity to work with Thomas Edison")
+    og.choiceArr[1].addChoice(newtitle: "Screw off! I want to stay here with my wife", newresult: "You live a long and fufilling life as a farmer")
+    og.choiceArr[1].choiceArr.append(og.choiceArr[0])
+    og.addChoice(newtitle: "Try your hand at a mechanical company", newresult: "You fail and go broke")
+    og.choiceArr[2].addChoice(newtitle: "Try Again", newresult: "You muster up an interview with two investors")
+    og.choiceArr[2].choiceArr.append(og.choiceArr[0])
+    og.choiceArr[2].choiceArr[0].addChoice(newtitle: "Make trains", newresult: "You absolutely suck at making trains, your wife leaves you, and you drink yourself to death")
+    og.choiceArr[2].choiceArr[0].addChoice(newtitle: "Make cars", newresult: "Start one of the biggest car companies in Detroit")
+    return og
 }
 
 
@@ -43,24 +53,35 @@ struct MyView: View {
     var curr: Choices
     var body: some View {
         VStack{
-            Text(curr.result)
+            Text(curr.result).font(
+                .custom(
+                "AmericanTypewriter",
+                fixedSize: 24).weight(.black)
+            ).padding(.bottom).frame(alignment: .top)
+            VStack{
             ForEach(curr.choiceArr) { el in
                 NavigationLink(destination: MyView(curr: el)) {
-                    Text(el.title).padding()
+                    Text(el.title).font(
+                        .custom(
+                        "AmericanTypewriter",
+                        fixedSize: 20)
+                    ).foregroundColor(.black).padding(.bottom)
                 }
             }
         }
+        }.frame(width: 300, height: 900, alignment: .center)
     }
 }
 
 struct newView: View {
-    var og: Choices = createChoiceTree()
+    var og: Choices = createHenryFordTree()
     var body: some View{
         NavigationView{
             VStack{
                 MyView(curr: og)
-            }.frame(width: 300, height: 900, alignment: .center)
+            }.frame(width: 400, height: 1000, alignment: .top)
         }
+        .accentColor(.black)
     }
 }
 
